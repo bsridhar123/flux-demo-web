54371b881487a38fd9485fcd23f133947f384263
Sridhar Balasubramanian <Sridhar Balasubramanian>
2017-05-27 00:38:48 +0530

    initial add

---
+++ src/main/java/com/example/demo/Router.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+
+@Configuration
+public class Router {
+
+	
+	@Bean
+	public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
+		return route(POST("/echo"), echoHandler::echo);
+	}
+}
